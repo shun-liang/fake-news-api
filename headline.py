97c320a8f3ac9685402d89b9eb6bdb6ea5551d30
Shun Liang <shun.liang.public@gmail.com>
2017-04-06 20:55:07 +0100

    Randomly shuffle nouns in news headlines

--- headline.py
+++ headline.py
@@ -2,6 +2,8 @@ import os
 import random
 import requests
 
+from textblob import TextBlob
+
 headlines = ['Headline 1', 'Headline 2']
 GUARDIAN_KEY = os.environ['GUARDIAN_KEY']
 
@@ -15,4 +17,26 @@ def get_guardian_news_headlines():
     assert r.status_code == 200
     r = r.json()
     headlines = [result['webTitle'] for result in r['response']['results']]
-    return headlines
+    blob_headlines = [TextBlob(headline) for headline in headlines]
+    return blob_headlines
+
+def process_blob_headlines(blob_headlines):
+    nouns = []
+    for blob_headline in blob_headlines:
+        for word, pos in blob_headline.tags:
+            if pos in ('NN', 'NNS'):
+                nouns.append(word)
+
+    random.shuffle(nouns)
+
+    fake_headlines = []
+    for blob_headline in blob_headlines:
+        fake_headline = []
+        for word, pos in blob_headline.tags:
+            if pos in ('NN', 'NNS'):
+                # replace noun in place
+                fake_headline.append(nouns.pop())
+            else:
+                fake_headline.append(word)
+        fake_headlines.append(fake_headline)
+    return fake_headlines
