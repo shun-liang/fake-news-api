5ca3cfd37c042316f84ed5eb803272663961370a
Shun Liang <shun.liang.public@gmail.com>
2017-04-06 20:21:01 +0100

    Return random headline from Guardian

--- headline.py
+++ headline.py
@@ -1,16 +1,16 @@
 import os
+import random
 import requests
 
 headlines = ['Headline 1', 'Headline 2']
 GUARDIAN_KEY = os.environ['GUARDIAN_KEY']
 
 def generate():
-    # return a fixed head line
-    return 'Fixed Head Line'
+    headline = random.choice(get_guardian_news_headlines())
+    return headline
 
 def get_guardian_news_headlines():
     guardian_url = 'https://content.guardianapis.com/search?api-key={0}'.format(GUARDIAN_KEY)
-    print guardian_url
     r = requests.get(guardian_url)
     assert r.status_code == 200
     r = r.json()
